84c4d8fdc146303baae5e51102d9408950b4d0c6
NCDinesh <deenez.npl1@gmail.com>
2025-01-21 22:09:29 +0545

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero </section>
+    <section className="padding">Popular Products</section>
+    <section className="padding">Super Quality</section>
+    <section className="padding">Services</section>
+    <section className="padding">Special Offer</section>
+  </main>
+);
+
+export default App;
